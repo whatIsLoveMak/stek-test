f2c29a946ec570ea65689bb1b13fbdea37b976de
whatIsLoveMak <2468_003@mail.ru>
2022-08-08 19:06:37 +0300

    fix bug for another computers

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,48 @@
   <div id="app">
     <div class="container">
       <div class="table-edit">
-        <t-input v-model="name" :placeholder="'Найти по ФИО'"/>
-        <t-button @click.native="openModal" :inner-text="'Добавить'"/>
+        <t-input v-model="name" :placeholder="'Найти по ФИО'" />
+        <t-button @click.native="openModal" :inner-text="'Добавить'" />
       </div>
       <div class="table">
         <t-table
-            @sort-name="sortName"
-            @sort-company="sortName"
-            @delete-item="deleteItem"
-            :items="searchName"
+          @sort-name="sortName"
+          @sort-company="sortName"
+          @delete-item="deleteItem"
+          :items="searchName"
         />
       </div>
       <div class="table-pagination">
         <div class="table-pagination__row">
-          <div  :class="{'disabled':page < 2}" @click="page = page - 1" class="table-pagination__row-item button">&#10148;</div>
-          <div class="table-pagination__row-item">Страница: {{page}}</div>
-          <div :class="{'disabled': showNextButton}" @click="page = page + 1" class="table-pagination__row-item button">&#10148;</div>
+          <div
+            :class="{ disabled: page < 2 }"
+            @click="page = page - 1"
+            class="table-pagination__row-item button"
+          >
+            &#10148;
+          </div>
+          <div class="table-pagination__row-item">Страница: {{ page }}</div>
+          <div
+            :class="{ disabled: showNextButton }"
+            @click="page = page + 1"
+            class="table-pagination__row-item button"
+          >
+            &#10148;
+          </div>
         </div>
       </div>
     </div>
     <transition name="fade">
       <t-modal
-          @close-modal="closeModal"
-          v-if="showModal"
-          @new-item="insertItem"
+        @close-modal="closeModal"
+        v-if="showModal"
+        @new-item="insertItem"
       />
     </transition>
-
   </div>
 </template>
 
 <script>
-
 import TInput from "@/components/UI/tInput";
 import TButton from "@/components/UI/tButton";
 import TTable from "@/components/tTable";
@@ -44,120 +54,123 @@ export default {
     TModal,
     TTable,
     TButton,
-    TInput
+    TInput,
   },
   data() {
     return {
       items: [
         {
           id: 1,
-          name: 'Иванов М.А.',
-          company: 'ООО Ростелеком1',
-          phone: '+7 (999) 999-99-99'
+          name: "Иванов М.А.",
+          company: "ООО Ростелеком1",
+          phone: "+7 (999) 999-99-99",
         },
         {
           id: 2,
-          name: 'Сидоров М.А.2',
-          company: 'ВВВ Ростелеком2',
-          phone: '+7 (999) 999-99-99'
+          name: "Сидоров М.А.2",
+          company: "ВВВ Ростелеком2",
+          phone: "+7 (999) 999-99-99",
         },
         {
           id: 3,
-          name: 'Ахматов М.А.3',
-          company: 'ААА Ростелеком3',
-          phone: '+7 (999) 999-99-99'
+          name: "Ахматов М.А.3",
+          company: "ААА Ростелеком3",
+          phone: "+7 (999) 999-99-99",
         },
         {
           id: 7,
-          name: 'Ахматов М.А.3',
-          company: 'ААА Ростелеком3',
-          phone: '+7 (999) 999-99-99'
+          name: "Ахматов М.А.3",
+          company: "ААА Ростелеком3",
+          phone: "+7 (999) 999-99-99",
         },
         {
           id: 4,
-          name: 'Ахматов М.А.3',
-          company: 'ААА Ростелеком3',
-          phone: '+7 (999) 999-99-99'
+          name: "Ахматов М.А.3",
+          company: "ААА Ростелеком3",
+          phone: "+7 (999) 999-99-99",
         },
         {
           id: 5,
-          name: 'Ахматов М.А.3',
-          company: 'ААА Ростелеком3',
-          phone: '+7 (999) 999-99-99'
+          name: "Ахматов М.А.3",
+          company: "ААА Ростелеком3",
+          phone: "+7 (999) 999-99-99",
         },
         {
           id: 6,
-          name: 'Ахматов М.А.3',
-          company: 'ААА Ростелеком3',
-          phone: '+7 (999) 999-99-99'
+          name: "Ахматов М.А.3",
+          company: "ААА Ростелеком3",
+          phone: "+7 (999) 999-99-99",
         },
       ],
       showModal: false,
       page: 1,
       total: 0,
-      name:'',
-      newArr:[],
+      name: "",
+      newArr: [],
       hasNextPage: true,
-    }
+    };
   },
   methods: {
     openModal() {
-      this.showModal = true
+      this.showModal = true;
     },
     closeModal(value) {
-      this.showModal = value
+      this.showModal = value;
     },
     insertItem(item) {
-      this.items.push(item)
-      localStorage.setItem('item', JSON.stringify(this.items))
-      this.showModal = false
+      this.items.push(item);
+      localStorage.setItem("item", JSON.stringify(this.items));
+      this.showModal = false;
     },
     deleteItem(id) {
-      this.items = this.items.filter((e) => e.id !== id)
-      if(this.items.length === 6){
-        this.page = 1
+      this.items = this.items.filter((e) => e.id !== id);
+      if (this.items.length === 6) {
+        this.page = 1;
       }
-      localStorage.setItem('item', JSON.stringify(this.items))
+      localStorage.setItem("item", JSON.stringify(this.items));
     },
     sortName(value) {
-      if(value === 'name'){
+      if (value === "name") {
         this.items.sort(function (a, b) {
-          return a.name.localeCompare(b.name)
-        })
+          return a.name.localeCompare(b.name);
+        });
       } else {
         this.items.sort(function (a, b) {
-          return a.company.localeCompare(b.company)
-        })
+          return a.company.localeCompare(b.company);
+        });
       }
-
-    }
+    },
   },
   computed: {
     searchName() {
-      const start = (this.page - 1) * 6
-      const end = this.page * 6
-      const filteredItems = this.items.filter(item => item.name.toLowerCase().includes(this.name.toLowerCase()))
-      return filteredItems.slice(start,end)
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+      const filteredItems = this.items.filter((item) =>
+        // ошибки выдавал .toLowerCase
+        // item.name.toLowerCase().includes(this.name.toLowerCase())
+        item.name.includes(this.name)
+      );
+      return filteredItems.slice(start, end);
     },
-    showNextButton(){
-      const end = this.page * 6
-      if(this.items.length > end) {
-        return false
+    showNextButton() {
+      const end = this.page * 6;
+      if (this.items.length > end) {
+        return false;
       }
-      return true
-    }
+      return true;
+    },
   },
   mounted() {
-    const itemsData = localStorage.getItem('item')
+    const itemsData = localStorage.getItem("item");
     if (itemsData) {
-      this.items = JSON.parse(itemsData)
+      this.items = JSON.parse(itemsData);
     }
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
-@import '@/assets/style/nullstyle.scss';
+@import "@/assets/style/nullstyle.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -180,8 +193,9 @@ export default {
     margin-bottom: 0px;
   }
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
   opacity: 0;
@@ -202,11 +216,11 @@ export default {
       width: 100%;
       transition: all 0.4s ease;
     }
-    &-item:first-child, &-item:last-child {
+    &-item:first-child,
+    &-item:last-child {
       cursor: pointer;
       padding: 10px 0px;
       border-radius: 5px;
-
     }
     &-item:first-child {
       transform: rotate(180deg);
--- src/components/tItem.vue
+++ src/components/tItem.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="table-grid__item">
-    <div class="table-grid__item-company">{{item.company}}</div>
-    <div class="table-grid__item-name">{{item.name}}</div>
-    <div class="table-grid__item-phone ">{{item.phone}}</div>
+    <div class="table-grid__item-company">{{ item.company }}</div>
+    <div class="table-grid__item-name">{{ item.name }}</div>
+    <div class="table-grid__item-phone">{{ item.phone }}</div>
     <div @click="deleteItem" class="table-grid__item-delete">&#10060;</div>
   </div>
 </template>
@@ -13,18 +13,15 @@ export default {
   props: {
     item: {
       type: Object,
-      default: () => {}
-    }
+      default: () => {},
+    },
   },
   methods: {
     deleteItem() {
-      console.log(this.item.id)
-      this.$emit('delete-item', this.item.id)
-    }
-  }
-}
+      this.$emit("delete-item", this.item.id);
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
